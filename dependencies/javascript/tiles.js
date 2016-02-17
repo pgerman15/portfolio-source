74488113221782aeb857fa65c5638f2c02d31af5
Paul German <paulbgerman@yahoo.com>
2016-02-17 16:09:21 -0600

    auto commit

--- dependencies/javascript/tiles.js
+++ dependencies/javascript/tiles.js
@@ -1,3 +1,9 @@
+$(document).ready(function(){
+	$('#newTileButton').click(function(){
+		$tile = $('<div class="col-xs-6 col-sm-3 col-lg-2"><div class="dummy"></div><a id="tile1" class="thumbnail>' + $('#tileText').val() + '</a>"</div>');
+	});
+});
+
 function createTile(tileLabel){
 	var newtile = document.createElement("div");
 	newtile.className = "col-xs-6 col-sm-3 col-lg-2";
