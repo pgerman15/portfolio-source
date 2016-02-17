bebe936e266398db8faac273f6b57ef0f117dff8
Paul German <paulbgerman@yahoo.com>
2016-02-17 12:34:09 -0600

    auto commit

--- dependencies/javascript/tiles.js
+++ dependencies/javascript/tiles.js
@@ -1,6 +1,6 @@
 function createTile(tileLabel){
 	var newtile = document.createElement("div");
 	newtile.className = "col-xs-6 col-sm-3 col-lg-2";
-	newtile.innerHTML="<div class='dummy'></div><a class='thumbnail'>" + tileLabel + "</a>"
+	newtile.innerHTML="<div class='dummy'></div><a id='tile1' class='thumbnail'>" + tileLabel + "</a>"
 	document.getElementById("mainRow").appendChild(newtile);
 }
