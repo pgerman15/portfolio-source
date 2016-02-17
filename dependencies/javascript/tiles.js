a962c95b1bb25d60be0de4cd48d81e2e8e365aca
Paul German <paulbgerman@yahoo.com>
2016-02-17 16:25:02 -0600

    auto commit

--- dependencies/javascript/tiles.js
+++ dependencies/javascript/tiles.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$('#newTileButton').click(function(){
 		var x = Math.floor(Math.random() * 10);
-		$tile = $('<div class="col-xs-6 col-sm-3 col-lg-2"><div class="dummy"></div><a id="tile' + count + '" class="thumbnail">' + $('#tileText').val() + '</a></div>');
+		$tile = $('<div class="col-xs-6 col-sm-3 col-lg-2"><div class="dummy"></div><a id="tile' + x + '" class="thumbnail">' + $('#tileText').val() + '</a></div>');
 		$('#mainRow').append($tile);
 		count += 1;
 	});
