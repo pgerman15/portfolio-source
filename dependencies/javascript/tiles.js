27a4e139f32751baf49002f509c02601c4353bcb
Paul German <paulbgerman@yahoo.com>
2016-02-18 09:37:05 -0600

    auto commit

--- dependencies/javascript/tiles.js
+++ dependencies/javascript/tiles.js
@@ -5,18 +5,13 @@ $(document).ready(function(){
 		$('#mainRow').append($tile);
 	});
 	
-	$('modalForm').submit(function(event){
-		event.preventDefault();
-		alert("ok then");
-		var y = Math.floor(Math.random() * 10);
-		$tile = $('<div class="col-xs-6 col-sm-3 col-lg-2"><div class="dummy"></div><a id="tile' + y + '" class="thumbnail">' + $('#tileText').val() + '</a></div>');
+
+	$('.input').keypress(function (e) {
+	  if (e.which == 13) {
+		var x = Math.floor(Math.random() * 10);
+		$tile = $('<div class="col-xs-6 col-sm-3 col-lg-2"><div class="dummy"></div><a id="tile' + x + '" class="thumbnail">' + $('#tileText').val() + '</a></div>');
 		$('#mainRow').append($tile);
+		return false;    //<---- Add this line
+	  }
 	});
 });
-
-function createTile(tileLabel){
-	var newtile = document.createElement("div");
-	newtile.className = "col-xs-6 col-sm-3 col-lg-2";
-	newtile.innerHTML="<div class='dummy'></div><a id='tile1' class='thumbnail'>" + tileLabel + "</a>"
-	document.getElementById("mainRow").appendChild(newtile);
-}
