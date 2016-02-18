19d1ae38b84a45f896e87fe6cef30de890891242
Paul German <paulbgerman@yahoo.com>
2016-02-18 09:37:44 -0600

    auto commit

--- dependencies/javascript/tiles.js
+++ dependencies/javascript/tiles.js
@@ -6,8 +6,9 @@ $(document).ready(function(){
 	});
 	
 
-	$('.input').keypress(function (e) {
+	$('input').keypress(function (e) {
 	  if (e.which == 13) {
+		alert("enter pressed");
 		var x = Math.floor(Math.random() * 10);
 		$tile = $('<div class="col-xs-6 col-sm-3 col-lg-2"><div class="dummy"></div><a id="tile' + x + '" class="thumbnail">' + $('#tileText').val() + '</a></div>');
 		$('#mainRow').append($tile);
