d4c658182f9a2493631803587e3a5dc23f909240
Paul German <paulbgerman@yahoo.com>
2016-02-22 10:45:02 -0600

    auto commit

--- dependencies/javascript/tiles.js
+++ dependencies/javascript/tiles.js
@@ -16,6 +16,16 @@ $(document).ready(function(){
 	  }
 	});
 	
+	$('#search').keypress(function (e) {
+	  if (e.which == 13) {
+		var x = Math.floor(Math.random() * 10);
+		$tile = $('<div class="col-xs-6 col-sm-3 col-lg-2"><div class="dummy"></div><a id="tile' + x + '" class="thumbnail">' + $('#tileText').val() + '</a></div>');
+		$('#mainRow').append($tile);
+		$('#myModal').modal('hide');
+		return false;    //<---- Add this line
+	  }
+	});
+	
 	$.get( "https://www.googleapis.com/youtube/v3/search?part=snippet&q=cats&key=AIzaSyDyk41ENXaIZ7uaM9YhXA7qN6afNDoSkBc&maxResults=10" ).then(
 	  function(data) {
 		alert( "$.get succeeded with data: " + JSON.stringify(data));
